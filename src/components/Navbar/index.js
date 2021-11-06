3fbd7f61f0218f2bc8b6caa23242b452bf70aff3
giannurwana19 <giannurwana19@gmail.com>
2021-11-06 16:02:33 +0700

    Navbar Component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import {
+  AppBar,
+  Badge,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import useStyles from './styles';
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" classes={classes.title}>
+            Gianpedia
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart item" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,5 +1,6 @@
 import { Grid } from '@material-ui/core';
 import Product from './Product';
+import useStyles from './styles';
 
 const products = [
   {
@@ -21,8 +22,11 @@ const products = [
 ];
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justifyContent="center" spacing={4}>
         {products.map(product => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
