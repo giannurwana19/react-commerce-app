def798fcca3870e9b10691e97f8b9e37cb4b3319
giannurwana19 <giannurwana19@gmail.com>
2021-11-08 18:52:59 +0700

    layout checkout form with react hook form

--- src/components/CheckoutForm/AddressForm/index.js
+++ src/components/CheckoutForm/AddressForm/index.js
@@ -1,5 +1,34 @@
+import {
+  InputLabel,
+  Select,
+  Button,
+  Grid,
+  Typography,
+} from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import FormInput from '../FormInput';
+
 const AddressForm = () => {
-  return <div>address form</div>;
+  const methods = useForm();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Alamat Pengiriman
+      </Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={''}>
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="Nama Depan" />
+            <FormInput required name="lastName" label="Nama Belakang" />
+            <FormInput required name="address" label="Alamat" />
+            <FormInput required name="email" label="Email" />
+            <FormInput required name="city" label="Kota" />
+            <FormInput required name="ZIP" label="Kode Pos" />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
--- src/components/CheckoutForm/Checkout/index.js
+++ src/components/CheckoutForm/Checkout/index.js
@@ -16,23 +16,22 @@ const Checkout = () => {
 
   return (
     <>
-      <div className={classes.toolbar}>
-        <main className={classes.layout}>
-          <Paper>
-            <Typography variant="h4" align="center">
-              Checkout
-            </Typography>
-            <Stepper activeStep={activeStep} className={classes.stepper}>
-              {steps.map(step => (
-                <Step key={step}>
-                  <StepLabel>{step}</StepLabel>
-                </Step>
-              ))}
-            </Stepper>
-            {activeStep === steps.length ? <Confirmation /> : <Form />}
-          </Paper>
-        </main>
-      </div>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map(step => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </main>
     </>
   );
 };
